3509a531996a5b138b447a8a1b50231141797cd0
relax1n <relaxinfh@mail.ru>
2018-05-14 09:55:07 +0300

    new

--- virtual/js/common.js
+++ virtual/js/common.js
@@ -11,6 +11,34 @@ $(function(){
 		autoplayHoverPause: true,
 		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
 	});
+		$('.partners').owlCarousel({
+		nav: true,
+		loop:true,
+		touchDrag: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		responsive:{
+			0:{
+				items:1
+			},
+			400:{
+				items:2
+			},
+			600:{
+				items:4
+			},
+			1200:{
+				items:5
+			},
+			
+
+		},
+
+		
+
+	});
 
 
 });
