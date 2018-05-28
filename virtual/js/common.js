400c42dc07b71926a7527a9c0aa22e04751b67d8
relax1n <relaxinfh@mail.ru>
2018-05-29 00:12:14 +0300

    c

--- virtual/js/common.js
+++ virtual/js/common.js
@@ -13,8 +13,8 @@ $(function(){
                     $(div) // берем стрoку с селектoрoм и делaем из нее jquery oбъект
                     .css('display', 'block')
                         .animate({opacity: 1, top: '40%'}, 200); // плaвнo пoкaзывaем
-                      });
-          });
+                    });
+        });
 
         close.click( function(){ // лoвим клик пo крестику или oверлэю
             modal // все мoдaльные oкнa
@@ -22,41 +22,46 @@ $(function(){
                     function(){ // пoсле этoгo
                     	$(this).css('display', 'none');
                         overlay.fadeOut(400); // прячем пoдлoжку
-                      }
-                      );
-              });
+                    }
+                    );
+            });
 
         $(".grouped_elements").fancybox({
           openEffect  : 'elastic',
           closeEffect : 'elastic',
 
-        });
+      });
 
 
 
         var state = true;
         $( ".b-seo__link-down" ).on( "click", function(e) {
             e.preventDefault();
-          if ( state ) {
-            $( ".seo-pre" ).animate({
-              height:  '100%'
-            }, 1000 );
-          } else {
-            $( ".seo-pre" ).animate({
+           
+            $(this).toggleClass("active");
 
-              height:  '200px'
-            }, 1000 );
-          }
-          state = !state;
+            if ( state ) {
+                $( ".seo-pre" ).animate({
+                    height:  '500px'
+              }, 1000 );
+
+            } else {
+                $( ".seo-pre" ).animate({
+
+                  height:  '200px'
+              }, 1000 );
+
+            }
+            state = !state;
         });
-     
 
-$('input[type="file"]').on('change', function (event, files, label) {
-  var file_name = this.value.replace(/\\/g, '/').replace(/.*\//, ''),
-  file_p = document.createElement('p');
-  var  deleteFile = document.createElement('a');
 
-  var  deleteI = document.createElement('i');
+        $('input[type="file"]').on('change', function (event, files, label) {
+          var file_name = this.value.replace(/\\/g, '/').replace(/.*\//, ''),
+          file_p = document.createElement('p');
+          var  deleteFile = document.createElement('a');
+
+          var  deleteI = document.createElement('i');
             $(deleteFile).addClass('deleteF '); //fa fa-times
             $(deleteFile).text(''); 
             $(file_p).addClass('file-name');
@@ -65,76 +70,76 @@ $('input[type="file"]').on('change', function (event, files, label) {
               $('.file-name').remove();
               $('.document').append(file_p); 
               $('.file-name').append(deleteFile);
-            }
-            else{
+          }
+          else{
               $('.document').append(file_p); 
               $('.file-name').append(deleteFile);
-            }
+          }
 
-          });
-$('.deleteF').click(function(e){
-  $(this).closest('.file-name').remove();
-});
+      });
+        $('.deleteF').click(function(e){
+          $(this).closest('.file-name').remove();
+      });
 
 
-$(".slider-range-online" ).slider({
- range: true,
- min: 10000,
- max: 5000100,
- values: [ 219493,   5000100 ],
- slide: function( event, ui ) {
-  $( ".budget__start" ).html( "ОТ" + " " + ui.values[0].toLocaleString() + '<span> руб </span>' );
-  if( ui.values[1] > 5000000 ){
-   $( ".budget__end" ).html(  "свыше " + "5 МЛН " + '<span> руб </span>'  );
- }
- else{
+        $(".slider-range-online" ).slider({
+           range: true,
+           min: 10000,
+           max: 5000100,
+           values: [ 219493,   5000100 ],
+           slide: function( event, ui ) {
+              $( ".budget__start" ).html( "ОТ" + " " + ui.values[0].toLocaleString() + '<span> руб </span>' );
+              if( ui.values[1] > 5000000 ){
+                 $( ".budget__end" ).html(  "свыше " + "5 МЛН " + '<span> руб </span>'  );
+             }
+             else{
 
-   $( ".budget__end" ).html(  "ДО" + " " + ui.values[ 1 ].toLocaleString() + '<span> руб </span>'  );
- }
-}
-});
+                 $( ".budget__end" ).html(  "ДО" + " " + ui.values[ 1 ].toLocaleString() + '<span> руб </span>'  );
+             }
+         }
+     });
 
-$("a.grouped_elements").fancybox({
-  'transitionIn'  : 'elastic',
-  'transitionOut' : 'elastic',
-  'speedIn'   : 600, 
-  'speedOut'    : 200, 
-  'overlayShow' : false
-});
+        $("a.grouped_elements").fancybox({
+          'transitionIn'  : 'elastic',
+          'transitionOut' : 'elastic',
+          'speedIn'   : 600, 
+          'speedOut'    : 200, 
+          'overlayShow' : false
+      });
 
 //$(".slider-range-online ").find('.ui-slider-handle:eq(-1)').css("background", "url('./img/onl-slid-btn-2.png')");
 
 $(".hamburger").click(function(){
- $(this).toggleClass("is-active");
- $('.nav').slideToggle("slow", function(){
- });
+   $(this).toggleClass("is-active");
+   $('.nav').slideToggle("slow", function(){
+   });
 });
 $('.scroll-b').slick({
- infinite: true,
- dots:true,
- arrows:false,
- autoplay:true,
- fade: true,
- autoplaySpeed: 15000,
- draggable:false,
+   infinite: true,
+   dots:true,
+   arrows:false,
+   autoplay:true,
+   fade: true,
+   autoplaySpeed: 15000,
+   draggable:false,
 });
 $('.price-slider').slick({
- infinite: true,
- dots:false,
- arrows:false,
- autoplay:true,
- fade: true,
- autoplaySpeed: 6000,
- draggable:false,
- pauseOnFocus: false,
- pauseOnHover : false,
+   infinite: true,
+   dots:false,
+   arrows:false,
+   autoplay:true,
+   fade: true,
+   autoplaySpeed: 6000,
+   draggable:false,
+   pauseOnFocus: false,
+   pauseOnHover : false,
 });
 
 $('#scroll-to-top').click(function () {
- $('body,html').animate({
-  scrollTop: 0
-}, 400);
- return false;
+   $('body,html').animate({
+      scrollTop: 0
+  }, 400);
+   return false;
 });
 
         /*$('.portfolio-sl').slick({
@@ -296,4 +301,4 @@ $('#scroll-to-top').click(function () {
         
         
 
-      });
+    });
