a16dfe885913a5916371fde66f3a4de882af89e2
relax1n <relaxinfh@mail.ru>
2019-11-01 11:19:04 +0200

    upd comons js and main.css

--- cube/js/common.js
+++ cube/js/common.js
@@ -1,35 +1,42 @@
 class MyCube{
+
 	constructor(wrapper , countRows , countCols){
 		this.createCube(wrapper,countRows,countCols);
 	}
+
 	createCube(wrapper ,countRows, countCols){ 	// Отрисовываем куб
 		this.wrapper = document.querySelector(wrapper);
-		this.btnRemoveRows = this.createElement('div',['cube__remove','cube__remove-row'] ,"<span>-</span>" , '.cube' );
-		this.btnRemoveCols = this.createElement('div',['cube__remove', 'cube__remove-item'] ,"<span>-</span>" , '.cube' );
-		this.btnAddCols = this.createElement('div',['cube__add', 'cube__add-item'] ,"<span>+</span>",'.cube');
-		this.btnAddRows = this.createElement('div',['cube__add', 'cube__add-row'] ,"<span>+</span>",'.cube' );
+		this.btnRemoveRows = this.createElement('div',['cube__remove','cube__remove-row'] ,"<span>-</span>" , this.wrapper );
+		this.btnRemoveCols = this.createElement('div',['cube__remove', 'cube__remove-item'] ,"<span>-</span>" , this.wrapper );
+		this.btnAddCols = this.createElement('div',['cube__add', 'cube__add-item'] ,"<span>+</span>",this.wrapper);
+		this.btnAddRows = this.createElement('div',['cube__add', 'cube__add-row'] ,"<span>+</span>",this.wrapper );
 		this.allRows = [];
 		this.allCols = [];
-		
+
 		for(let i = 0 ; i < countRows ; i++ ){
-			this.allRows.push(this.createElement('div', 'cube__row',null,'.cube'));
+			this.allRows.push(this.createElement('div', 'cube__row',null,this.wrapper));
 			for(let j = 0; j < countCols ; j++){
-				this.allCols.push(this.createElement('div', 'cube__item',null,'.cube__row'));
+				this.allCols.push(this.createElement('div', 'cube__item',null,this.allRows));
 			}
 		}
+
 		this.marginTopCols = parseInt(getComputedStyle(this.allCols[0],true).marginTop);
 		this.paddingWrapper = parseInt(getComputedStyle(this.wrapper,true).paddingLeft);
 
 		this.createAttrInRowsAndCols();
+
 		this.wrapper.addEventListener('mouseover', this.showBtn.bind(this));
 		this.wrapper.addEventListener('mouseover', this.setAttrInBtnRemoveColsRow.bind(this));
+		this.wrapper.addEventListener('mouseleave',  this.mouseOutFromWrapper.bind(this));
+		this.wrapper.addEventListener('mouseover', this.setCoordinatesForBtnRemoveColsRows.bind(this));
+
 		this.btnAddRows.addEventListener('click', this.addRow.bind(this , this.createAttrInRowsAndCols.bind(this)));
 		this.btnAddCols.addEventListener('click', this.addCol.bind(this));
+
 		this.btnRemoveCols.addEventListener('click', this.removeCols.bind(this));
 		this.btnRemoveRows.addEventListener('click', this.removeRows.bind(this));
-		this.wrapper.addEventListener('mouseout',  this.hiddenBtn.bind(this));
-		this.wrapper.addEventListener('mouseover', this.setCoordinatesForBtnRemoveColsRows.bind(this));
 	}
+
 	createAttrInRowsAndCols(){ 	// Создаем атрибуты
 		for(let i = 0; i < this.allRows.length; i++){
 			this.allRows[i].setAttribute('data-number',i);
@@ -39,7 +46,25 @@ class MyCube{
 		}
 	}
 
-	hiddenBtn(){	//прячем кнопки
+	mouseOutFromWrapper(){	//прячем кнопки
+		let timer = setTimeout(()=>{
+			this.hiddeBtn();
+		},300);
+
+		this.btnRemoveCols.addEventListener('mouseenter',() =>{
+			clearTimeout(timer);
+		});
+
+		this.btnRemoveRows.addEventListener('mouseenter',() =>{
+			clearTimeout(timer);
+		});
+		
+		this.wrapper.addEventListener('mouseenter',()=>{
+			clearTimeout(timer);
+		})
+	}
+
+	hiddeBtn(){
 		this.btnRemoveRows.classList.remove('active');
 		this.btnRemoveCols.classList.remove('active');
 	}
@@ -47,7 +72,7 @@ class MyCube{
 	createElement(name  , classes , innerHtml = null , parents){  //Функция для создания элемента
 		this.element = document.createElement(name);
 		this.classAll = classes;
-		this.wrapBlock = document.querySelectorAll(parents);
+		this.wrapBlock = parents;
 		if(typeof this.classAll === 'object'){
 			for(let i = 0 ; i < this.classAll.length ; i++){
 				this.element.classList.add(this.classAll[i]);
@@ -57,9 +82,15 @@ class MyCube{
 			this.element.classList.add(this.classAll);
 		}
 		this.element.innerHTML = innerHtml;
-		for(let i = 0; i < this.wrapBlock.length; i++){
-			this.wrapBlock[i].appendChild(this.element);
+		if(this.wrapBlock == this.wrapper){
+			this.wrapBlock.appendChild(this.element);
 		}
+		else{
+			for(let i = 0; i < this.wrapBlock.length; i++){
+				this.wrapBlock[i].appendChild(this.element);
+			}
+		}
+
 		return this.element;
 	}
 
@@ -78,6 +109,7 @@ class MyCube{
 			})
 		}
 	}
+
 	setAttrInBtnRemoveColsRow(){  // Определяем атрибуты для кнопок удлаения
 		if(event.target.classList.contains('cube__item')){
 			this.attrCol = event.target.getAttribute('data-number');
@@ -86,28 +118,37 @@ class MyCube{
 			this.btnRemoveRows.setAttribute('data-number',this.attrRow);
 		}
 	}
+
 	setCoordinatesForBtnRemoveColsRows(){ // координаты для кнопок удаления
 		if(event.target.classList.contains('cube__item')){
 			this.btnRemoveRows.style.top = this.marginTopCols + event.target.parentElement.offsetTop + 'px';
 			this.btnRemoveCols.style.left = this.paddingWrapper + event.target.offsetLeft + 'px';
 		}
 	}
+
 	addRow(){ 	// Добавляем ряд
 		this.allRows.push(this.wrapper.appendChild(this.allRows[0].cloneNode(true)));
 		for(let i = 0; i<this.allRows[0].childElementCount;i++){
 			this.allCols.push(this.allCols[i].cloneNode(true));
 		}
+
 		this.createAttrInRowsAndCols();
+
 	}
+
 	addCol(){	// Добавляем колонку
 		for(let i = 0 ; i < this.allRows.length; i++){
 			let cols = this.allRows[i].appendChild(this.allCols[0].cloneNode(true));
 			this.allCols.push(cols);
 		}
+
 		this.createAttrInRowsAndCols();
+
 	}
+
 	removeCols(){	// Удаляем колонку
 		this.btnRemoveCols.classList.remove('active');
+
 		for(let i=0; i<this.allRows.length;i++){
 			for(let j = 0; j < this.allRows[i].childNodes.length ; j++){
 				if(this.btnRemoveCols.getAttribute('data-number') == this.allRows[i].childNodes[j].getAttribute('data-number')){
@@ -118,18 +159,26 @@ class MyCube{
 				}
 			}
 		}
+
 		this.createAttrInRowsAndCols();
+
 	}
+
 	removeRows(){ // Удаляем ряд
 		this.btnRemoveRows.classList.remove('active');
 		this.allRows.forEach( (item) =>{
 			if(this.btnRemoveRows.getAttribute('data-number') == item.getAttribute('data-number') ){
 				item.remove();
+
 				let index = this.allRows.indexOf(item);
 				this.allRows.splice(index,1);
+
 			}
 		},true);
+
 		this.createAttrInRowsAndCols();
+
 	}
 }
+
 let cube = new MyCube('.cube' , 4, 4);
