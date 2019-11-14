b0e3f30014697063943290c0bdab8d8d5a21443d
relax1n <relaxinfh@mail.ru>
2019-11-14 15:16:14 +0200

    fix header bug.

--- landing/js/common.js
+++ landing/js/common.js
@@ -1,8 +1,11 @@
 window.onload = function() {
 
   let headerTop = document.querySelector('.header__top-wrapper');
-  
+  let headerContainer = document.querySelector('.header__container');
+  let headerContainerHeight = headerContainer.clientHeight;
+
   window.addEventListener('scroll' , () => {
+    headerContainer.style.height = headerContainerHeight + 'px';
     if(document.documentElement.scrollTop > 0){
       headerTop.classList.add('fixed');
     }
