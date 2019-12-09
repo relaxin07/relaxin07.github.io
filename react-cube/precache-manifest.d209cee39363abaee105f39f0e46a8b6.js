6f8ae245470a2d2209046eeca24e962b7a2c6e8b
relax1n <relaxinfh@mail.ru>
2019-12-09 13:44:23 +0200

    new app

---
+++ react-cube/precache-manifest.d209cee39363abaee105f39f0e46a8b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715b9b440e77be05d7f479930c0ac938",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da0e1030a0182bc8283d",
+    "url": "/static/css/main.63849f8c.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da0e1030a0182bc8283d",
+    "url": "/static/js/main.a291af85.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
